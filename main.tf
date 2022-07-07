263fe2e56d8fb8dba924d7639894fedaf368acf6
Andy-ch <andy.v.che@gmail.com>
2022-07-08 01:39:28 +0200

    Add monthly budget

--- main.tf
+++ main.tf
@@ -5,3 +5,11 @@ resource "aws_budgets_budget" "daily" {
   limit_unit   = "USD"
   time_unit    = "DAILY"
 }
+
+resource "aws_budgets_budget" "monthly" {
+  name         = "monthly-budget"
+  budget_type  = "COST"
+  limit_amount = var.monthly_limit
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+}
