a8db8db21cf8011d91f8560dee7d5364be4827ac
Andy-ch <andy.v.che@gmail.com>
2022-07-08 21:30:38 +0200

    Create precommit script, which also creates a pre-commit Git hook on itself

--- scripts/fmt.sh
+++ scripts/fmt.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 set -xe
+mv secrets.auto.tfvars secrets.auto.tfvars.enc
 docker run --rm -v "$(pwd):/project" -w /project hashicorp/terraform fmt
+mv secrets.auto.tfvars.enc secrets.auto.tfvars
